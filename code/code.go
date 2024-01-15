117a38557a7be7689fcea3017c0a97441e164c10
Joost van der Griendt <joostvdg@gmail.com>
2024-01-15 22:56:41 +0100

    add infix and prefix operators

--- code/code.go
+++ code/code.go
@@ -67,6 +67,8 @@ const (
 	OpEqual
 	OpNotEqual
 	OpGreaterThan
+	OpMinus
+	OpBang
 )
 
 type Definition struct {
@@ -86,6 +88,8 @@ var definitions = map[Opcode]*Definition{
 	OpEqual:       {"OpEqual", []int{}},
 	OpNotEqual:    {"OpNotEqual", []int{}},
 	OpGreaterThan: {"OpGreaterThan", []int{}},
+	OpMinus:       {"OpMinus", []int{}},
+	OpBang:        {"OpBang", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
